c21f9fa62335b0d3140c7f76bf5d9473ef00c97d
Yousef Shaban <yousefshaban.eg@gmail.com>
2022-12-09 13:43:34 +0200

    Book Init Structure.

---
+++ Tasks/Task 5 - Day 5 - Book App/app/app.js
@@ -0,0 +1,23 @@
+//Requires
+const express = require("express");
+const path = require("path");
+const hbs = require("hbs");
+
+//Variables.
+const app = express();
+const staticFiles = path.join(__dirname, "../clientSide/static");
+const viewsFiles = path.join(__dirname, "../clientSide/views");
+const layoutFiles = path.join(__dirname, "../clientSide/layouts");
+
+//Logic
+app.use(express.static(staticFiles));
+app.set("view engine", "hbs");
+app.set("views", viewsFiles);
+hbs.registerPartials(layoutFiles);
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+module.exports = app;
